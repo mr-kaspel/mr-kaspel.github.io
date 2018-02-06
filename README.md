89c62905c7a48b3982b3ae5a1b68a3867089cc0b
Andrew <EndryNiksan@yandex.ru>
2018-02-06 22:07:21 +0800

    added saving tables

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ Even after accidental closing, you will not lose any progress.
 ```
 > convenient timer for tracking working hours.
 
+> local storage is cleaned automatically after sixteen hours.
+
+> also, local storage can be cleaned manually by clicking the clear button.
+
 ![alt text](/img/scrin_one.PNG)
--- js/timemachine.js
+++ js/timemachine.js
@@ -1,7 +1,4 @@
 'use strict';
-//console.log(new Date().getSeconds());
-var click = 0;
-
 
 /*Скрыть колонку*/
 function hidingСolumn(){
@@ -25,9 +22,9 @@ function tuning(){
 
 	this.timePassed = function(rm) {
 		rm = rm.split(';');
-		let hr = data.getHours() - rm[0].split(':')[0] + +rm[1].split(':')[0];
-		let min = data.getMinutes() - rm[0].split(':')[1] + +rm[1].split(':')[1];
-		let sec = data.getSeconds() - rm[0].split(':')[2] + +rm[1].split(':')[2];
+		let hr = data.getHours() - rm[0].split(':')[0] + +rm[1].split(':')[0],
+				min = data.getMinutes() - rm[0].split(':')[1] + +rm[1].split(':')[1],
+				sec = data.getSeconds() - rm[0].split(':')[2] + +rm[1].split(':')[2];
 		return hr + ":" + min + ":" + sec;
 	}
 
@@ -37,8 +34,27 @@ function tuning(){
 			var coll = document.getElementById('table').children;
 			var clon = coll[coll.length - 1].cloneNode(true);
 			clon.children[1].innerHTML = document.getElementById('hr').innerHTML + ':' + document.	getElementById('min').innerHTML;
-			clon.children[0].innerHTML = 1;
+			clon.children[0].children[0].value = 1;
+
 			return coll[coll.length - 1].parentNode.insertBefore(clon, coll[coll.length - 1].nextSibling);
+		}
+	}
+
+	this.saveTable = function() {
+		var qt = document.getElementById('table').children;
+		var textDump = '';
+		for(var i = 2; i < qt.length; i++) {
+			let a = qt[i].children[0].children[0].value;
+			let b = qt[i].children[1].innerHTML;
+			textDump += a + ";" + b + "/";
+		}
+
+		try {
+			localStorage.setItem('tableDump', textDump);
+		} catch (e) {
+			if (e == QUOTA_EXCEEDED_ERR) {
+				alert('>5mb');
+			}
 		}
 	}
 
@@ -62,6 +78,42 @@ function tuning(){
 		}
 		return document.getElementById('nav-menu').style.display = "";
 	}
+
+	this.timer = function() {
+		var sec = 0,
+				min = 0,
+				hr = 0;
+		var settime = setInterval(function() {
+			if (sec < 60) sec++;
+			if (sec === 60) {
+				min++;
+				sec = 0;
+			}
+			if(min == 60) {
+				hr++;
+				min = 0;
+				sec = 0;
+			}
+			if(click > 1) {
+				clearInterval(settime);
+				return click = 1;
+			}
+
+			/*Запись в локальное хранилище + проверка на переполнение*/
+			/*Только вот на секунду отстает*/
+			try {
+				localStorage.setItem('dateDump', opt.realTime() + opt.timerTime());
+			} catch (e) {
+				if (e == QUOTA_EXCEEDED_ERR) {
+					alert('>5mb');
+				}
+			}
+
+			return document.getElementById('sec').innerHTML = sec,
+			document.getElementById('min').innerHTML = min,
+			document.getElementById('hr').innerHTML = hr;
+		}, 1000);
+	}
 }
 
 var opt = new tuning();
@@ -72,11 +124,10 @@ if(!opt.checkCookies('pampam')) {
 	document.cookie = "pampam=value; path=/; expires=" + date.toUTCString();
 }
 
-
 /*Провека данных в хранилище*/
 let dateTime = localStorage.getItem('dateDump');
 if(dateTime) {
-	console.log(opt.timePassed(dateTime));
+	console.log("Time since last visit + recorded time: " + opt.timePassed(dateTime));
 }
 
 /*Проверка в локальном хранилище параметров*/
@@ -88,55 +139,29 @@ if(optio) {
 	}
 }
 
+var click = 0;
+
 /*Отслеживаем все нажатия*/
 document.body.addEventListener('keydown', function(e) {
 	e = e || window.event;
 	let keyup = e.keyCode;
-	if(keyup == 17) {
-		if (keyup === 32) {
-			console.log('ye');
-		}
-	}
+
 	//убрать 123
 	if(keyup !== 123 && keyup !== 116) e.preventDefault();
 
 	if(keyup === 32) {
 		click ++;
-		var sec = 0,
-				min = 0,
-				hr = 0;
+		if(click >= 1){
+			opt.timer();
+			/*Задержка для определение новых узлов*/
+			setTimeout(function()
+				{ opt.saveTable();
+			}, 500);
 
-		if(click === 1){
-			var settime = setInterval(function() {
-				if (sec < 60) sec++;
-				if (sec === 60) {
-					min++;
-					sec = 0;
-				}
-				if(min == 60) {
-					hr++;
-					min = 0;
-					sec = 0;
-				}
-				if(click > 1) {
-					clearInterval(settime);
-					return click = 0;
-				}
-				document.getElementById('sec').innerHTML = sec;
-				document.getElementById('min').innerHTML = min;
-				document.getElementById('hr').innerHTML = hr;
-
-				/*Запись в локальное хранилище + проверка на переполнение*/
-				try {
-					localStorage.setItem('dateDump', opt.realTime() + opt.timerTime());
-				} catch (e) {
-					if (e == QUOTA_EXCEEDED_ERR) {
-						alert('>5mb');
-					}
-				} 
-			}, 1000);
+			
 		}
 		opt.editTable(click);
+		console.log("number pressing: " + click);
 	}
 });
 
@@ -149,7 +174,8 @@ document.getElementById('point').addEventListener('click', function(){
 /* Чистка таблицы и Local Storage*/
 document.getElementById('clear').addEventListener('click', function(){
 	localStorage.removeItem('dateDump');
-	//localStorage.clear();
+	localStorage.removeItem('tableDump');
+	opt.clearStorage();
 	console.log('Значение из хранилища, с ключем dateDump, отчищено!');
 });
 
